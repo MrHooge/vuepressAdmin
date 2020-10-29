2892b3c33b658fba8d2356fc6da45f7eb332ea56
MrHooge <yunfei6558@qq.com>
2020-10-29 21:39:43 +0800

    更新

--- admin/src/layouts/AdminLayout.vue
+++ admin/src/layouts/AdminLayout.vue
@@ -1,15 +1,86 @@
 <template>
-<div></div>
+<a-layout id="components-layout-demo-side" style="min-height: 100vh">
+    <a-layout-sider v-model:collapsed="collapsed" collapsible>
+        <div class="logo" />
+        <a-menu theme="dark" v-model:selectedKeys="selectedKeys" mode="inline">
+            <a-menu-item key="1">
+                <pie-chart-outlined />
+                <span>Option 1</span>
+            </a-menu-item>
+            <a-menu-item key="2">
+                <desktop-outlined />
+                <span>Option 2</span>
+            </a-menu-item>
+            <a-sub-menu key="sub1">
+                <template v-slot:title>
+                    <span>
+                        <user-outlined /><span>User</span>
+                    </span>
+                </template>
+                <a-menu-item key="3">Tom</a-menu-item>
+                <a-menu-item key="4">Bill</a-menu-item>
+                <a-menu-item key="5">Alex</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub2">
+                <template v-slot:title>
+                    <span>
+                        <team-outlined /><span>Team</span>
+                    </span>
+                </template>
+                <a-menu-item key="6">Team 1</a-menu-item>
+                <a-menu-item key="8">Team 2</a-menu-item>
+            </a-sub-menu>
+            <a-menu-item key="9">
+                <file-outlined />
+                <span>File</span>
+            </a-menu-item>
+        </a-menu>
+    </a-layout-sider>
+    <a-layout>
+        <a-layout-header style="background: #fff; padding: 0" />
+        <a-layout-content style="margin: 0 16px">
+            <a-breadcrumb style="margin: 16px 0">
+                <a-breadcrumb-item>User</a-breadcrumb-item>
+                <a-breadcrumb-item>Bill</a-breadcrumb-item>
+            </a-breadcrumb>
+            <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
+                Bill is a cat.
+            </div>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+            Ant Design ©2018 Created by Ant UED
+        </a-layout-footer>
+    </a-layout>
+</a-layout>
 </template>
 
 <script lang="ts">
 import {
     defineComponent
 } from 'vue';
+import {
+    PieChartOutlined,
+    DesktopOutlined,
+    UserOutlined,
+    TeamOutlined,
+    FileOutlined,
+} from '@ant-design/icons-vue';
 
 export default defineComponent({
+    components: {
+        PieChartOutlined,
+        DesktopOutlined,
+        UserOutlined,
+        TeamOutlined,
+        FileOutlined,
+    },
     setup() {
-        return {};
+        const collapsed: boolean = false;
+        const selectedKeys: Array < string > = ['1'];
+        return {
+            collapsed,
+            selectedKeys
+        };
     },
 });
 </script>
---
+++ admin/src/layouts/header/header.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    Header
+</div>
+</template>
+
+<script lang="ts">
+import {
+    defineComponent
+} from 'vue'
+
+export default defineComponent({
+    setup() {
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
